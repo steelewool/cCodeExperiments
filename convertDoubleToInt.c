d4ec1eedb45b8caa0cda0a00f6652423537199a0
Rob Steele <robert@steelewoolsoftware.com>
2018-07-12 12:06:06 -0700

    Both versions working for positive and negative values.

--- convertDoubleToInt.c
+++ convertDoubleToInt.c
@@ -1,69 +1,33 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <stdint.h>
+#include <inttypes.h>
 
-int main (int argc, char** argv) {
-
-  typedef union {
-    double f;
-    struct {
-      uint64_t mantissa : 52;
-      uint64_t exponent : 11;
-      uint64_t sign     :  1;
-    } parts;
-    struct {
-      uint64_t hex : 64;
-    };
-  } double_cast;
-  
-  double_cast dc;
-
-  long unsigned int mantissa;
-  int exponent;
-  int sign;
-
-  for (int i = 1; i < argc; ++i)
-    printf ("argument: %d: %s\n", i, argv[i]);
-
-  dc.f = (double)atof(argv[1]);
-  dc.f = (double)strtod(argv[1], NULL);
-
-  printf ("value: %g\n", dc.f);
-  printf ("value as hex: 0x%lx: \n", dc.hex);
-
-  printf ("sign: %d ",      dc.parts.sign);
-  printf ("mantessa: %lu ", (long unsigned int)dc.parts.mantissa);
-  printf ("exponent: %d\n", dc.parts.exponent);
-
-  printf ("sign: 0x%x mantesia: 0x%lx exponent: 0x%x \n",
-	  dc.parts.sign,
-	  (long unsigned int)dc.parts.mantissa,
-	  dc.parts.exponent);
-
-  sign     = ((uint64_t)dc.f & 0x8000000000000000L) != 0; 
-  exponent = ((uint64_t)dc.f & 0x7ff0000000000000L) >> 52;
-  mantissa =  (uint64_t)dc.f & 0x000fffffffffffffL;
-
-  printf ("sign: %d mantissa: %lu exponent: %d \n",
-	  sign,
-	  mantissa,
-	  exponent);
-
-  printf ("sign: 0x%x mantissa: 0x%lx exponent: 0x%x \n",
-	  sign,
-	  mantissa,
-	  exponent);
+int main (int argc, char **argv) {
 
   /*
-  argument1 = strtol(argv[1], 0, 16);
-  argument2 = strtol(argv[2], 0, 16);
-
-  printf ("argument1: %d argument2: %d\n", argument1, argument2);
-
-  value = (double) argument1  * pow((double)10,(double)argument2);
+   *exp = (arg == 0) ? 0 : (int)(1 + std::log10(std::fabs(arg) ) );
+   return arg * pow(10 , -(*exp));    
   */
 
-  return 0;
+  double value;
+  int    exponent;
+  double mantissa;
+  int    intMantissa;
+
+  value = atof(argv[1]);
+
+  printf ("value: %g \n", value);
+
+  exponent = (value == 0) ? 0 : (int)(/* 1 */ -9 + log10(fabs(value)));
+  mantissa = value * pow(10,-(exponent));
+  intMantissa = mantissa;
+
+  printf ("exponent:    %d \n",   exponent);
+  printf ("exponent:    0x%8x \n", exponent);
+  printf ("mantissa:    %g \n",   mantissa);
+  printf ("intMantissa: %d \n",   intMantissa);
+  printf ("intMantissa: 0x%8x \n", intMantissa);
+
 }
--- convertIntToDouble.c
+++ convertIntToDouble.c
@@ -8,9 +8,6 @@ int main (int argc, char** argv) {
   int argument1, argument2;
   double value;
 
-  for (int i = 1; i < argc; ++i)
-    printf ("argument: %d: %s\n", i, argv[i]);
-
   argument1 = strtol(argv[1], 0, 16);
   argument2 = strtol(argv[2], 0, 16);
 
